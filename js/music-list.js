e9897303e2fb6daac2ec5d16cebd7fcbd55f5435
latiffanjaya <jumaansandal.@gmail.com>
2024-09-18 08:24:16 +0700

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,92 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "berlayar",
+    artist: "nicolas",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Aku coba merayu tuhan",
+    artist: "dj surabaya",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "suci dalam debu",
+    artist: "dj surbaya",
+    img: "music-9",
+    src: "music-9"
+  }, 
+  {
+    name: "FULL ALBUM __ Az-zahir Pekalongan __ TERBARU 2023",
+    artist: "AZAHIR  ",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+  name: "MAHALLUL QIYAM MERDU MENYENTUH HATI - MAULID AL BARZANJI",
+  artist: "Az-zahir",
+  img: "music-11",
+  src: "music-11"
+  },
+  {
+name: "Sholawat Viral Terbaru - Ahbab Rosulillah Azzahir",
+artist: "Az-zahir",
+img: "music-12",
+src: "music-12"
+  }
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
